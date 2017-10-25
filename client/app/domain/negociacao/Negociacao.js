4878e97ceb85fd384e9f2d5910e1a8ccaf78bd06
thallesManjaterra <thalles.manjaterra@gmail.com>
2017-10-25 15:15:23 +0000

    Programação defensiva - tornando a data de uma negociação realmente imutável.

--- client/app/domain/negociacao/Negociacao.js
+++ client/app/domain/negociacao/Negociacao.js
@@ -4,7 +4,7 @@ Serve como um aviso, dizendo:
 "Programador, você não pode alterar esta propriedade!". */
 class Negociacao {
     constructor(data, qtd, valor) {
-        this._data = data;
+        this._data = new Date(data.getTime());
         this._quantidade = qtd;
         this._valor = valor;
         Object.freeze(this);
@@ -14,7 +14,9 @@ class Negociacao {
         return this._quantidade * this._valor;
     }
     get data() {
-        return this._data;
+        return new Date(this._data.getTime());
+        /*O getTime() de uma data retornará um número long
+        com uma representação da data*/
     }
     get quantidade() {
         return this._quantidade;
