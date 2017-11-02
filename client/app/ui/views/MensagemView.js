d9b40a8ae927393d0468baaf147a6554644450f7
thallesManjaterra <thalles.manjaterra@gmail.com>
2017-11-02 14:52:25 +0000

    Herança e reutilização de código - classes MensagemView e NegociacoesView extends View.

--- client/app/ui/views/MensagemView.js
+++ client/app/ui/views/MensagemView.js
@@ -1,11 +1,5 @@
-class MensagemView {
-    constructor(seletor) {
-        this._elemento = document.querySelector(seletor);
-    }
-    update(model) {
-        this._elemento.innerHTML = this._template(model);
-    }
-    _template(model) {
+class MensagemView extends View{
+    template(model) {
         return model.texto
         ? `<p class="alert alert-info">${model.texto}</p>`
         : `<p></p>`;
