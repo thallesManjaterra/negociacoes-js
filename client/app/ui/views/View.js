badbb822ccec690aa8f90ff83432f30c464b9356
thallesManjaterra <thalles.manjaterra@gmail.com>
2017-11-02 14:59:07 +0000

    Emulando classe abstrata no javascript.

--- client/app/ui/views/View.js
+++ client/app/ui/views/View.js
@@ -5,4 +5,7 @@ class View {
     update(model) {
         this._elemento.innerHTML = this.template(model);
     }
+    template(model) {
+        throw new Error("Você precisa implementar o método template");
+    }
 }
