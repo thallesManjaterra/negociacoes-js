83607cf1a42df44245338a65f8ce8aa7fe5c4dae
thallesManjaterra <thalles.manjaterra@gmail.com>
2017-11-03 12:51:40 +0000

    Utilizando padrão de projeto factory para auxiliar implementação do proxy.

--- client/app/controllers/NegociacaoController.js
+++ client/app/controllers/NegociacaoController.js
@@ -1,31 +1,21 @@
 class NegociacaoController {
     constructor() {
         let $ = document.querySelector.bind(document);
-        const self = this; // ou usar arrow function:
-        /*o escopo de seu this é léxico (estático) em vez de
-        dinâmico. O this de uma arrow function obtém seu valor do "código ao
-        redor", mantendo esse valor independente do lugar onde é
-        chamado.*/
         Object.assign(this, {
             _inputData: $('#data'),
             _inputQuantidade: $('#quantidade'),
             _inputValor: $('#valor'),
-            _negociacoes: new Proxy( new Negociacoes(), {
-                get(target, prop, receiver) {
-                    if(typeof(target[prop]) == typeof(Function) &&
-                        ['adiciona', 'esvazia'].includes(prop)){
-                            return function () {
-                                console.log(`"${prop}" disparou armadilha!`);
-                                target[prop].apply(target, arguments)
-                                self._negociacoesView.update(target);
-                            }
-                        } else {
-                            return target[prop];
-                        }
-                }
-            }),
+            _negociacoes: ProxyFactory.create(
+                new Negociacoes(),
+                ['adiciona', 'esvazia'],
+                target => this._negociacoesView.update(target)
+            ),
             _negociacoesView: new NegociacoesView('#negociacoes'),
-            _mensagem: new Mensagem(),
+            _mensagem: ProxyFactory.create(
+                new Mensagem(),
+                ['texto'],
+                target => this._mensagemView.update(target)
+            ),
             _mensagemView: new MensagemView('#mensagemView')
         });
         this._negociacoesView.update(this._negociacoes);
@@ -35,13 +25,11 @@ class NegociacaoController {
         event.preventDefault();
         this._negociacoes.adiciona(this._criarNegociacao());
         this._mensagem.texto = 'Negociação adicionada com sucesso';
-        this._mensagemView.update(this._mensagem);
         this._limpaFormulario();
     }
     apaga() {
         this._negociacoes.esvazia();
         this._mensagem.texto = "Negociacoes apagadas com sucesso";
-        this._mensagemView.update(this._mensagem);
     }
     _limpaFormulario() {
         this._inputData.value = '',
