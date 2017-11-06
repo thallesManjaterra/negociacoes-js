a2cacf39f873bb4e4b374e6c9c0b29a2f193aaa6
thallesManjaterra <thalles.manjaterra@gmail.com>
2017-11-06 11:06:35 +0000

    Garantindo uma conexão apenas por aplicação, utilizando o padrão de projeto Module Pattern.

--- client/app/util/ConnectionFactory.js
+++ client/app/util/ConnectionFactory.js
@@ -1,29 +1,33 @@
-const stores = ['negociacoes'];
-
-class ConnectionFactory {
-    constructor() {
-        throw new Error('Não é possível criar instâncias dessa classe.');
+const ConnectionFactory = (() => {
+    const stores = ['negociacoes'];
+    let connection = null;
+    return class ConnectionFactory {
+        constructor() {
+            throw new Error('Não é possível criar instâncias dessa classe.');
+        }
+        static getConnection() {
+            return new Promise((resolve, reject) => {
+                if(connection) return resolve(connection);
+                const openRequest = indexedDB.open('jscangaceiro', 2);
+                openRequest.onupgradeneeded = e => {
+                    ConnectionFactory._createStores(e.target.result);
+                };
+                openRequest.onsuccess = e => {
+                    connection = e.target.result
+                    resolve(e.target.result)
+                };
+                openRequest.onerror = e => {
+                    console.log(e.target.error);
+                    reject(e.target.error.name)
+                };
+            });
+        }
+        static _createStores(connection) {
+            stores.forEach(store => {
+                if(connection.objectStoreNames.contains(store))
+                    connection.deleteObjectStore(store);
+                connection.createObjectStore(store, {autoIncrement: true});
+            });
+        }
     }
-    static getConnection() {
-        return new Promise((resolve, reject) => {
-            const openRequest = indexedDB.open('jscangaceiro', 2);
-            openRequest.onupgradeneeded = e => {
-                ConnectionFactory._createStores(e.target.result);
-            };
-            openRequest.onsuccess = e => {
-                resolve(e.target.result)
-            };
-            openRequest.onerror = e => {
-                console.log(e.target.error);
-                reject(e.target.error.name)
-            };
-        });
-    }
-    static _createStores(connection) {
-        stores.forEach(store => {
-            if(connection.objectStoreNames.contains(store))
-                connection.deleteObjectStore(store);
-            connection.createObjectStore(store, {autoIncrement: true});
-        });
-    }
-}
+})();
