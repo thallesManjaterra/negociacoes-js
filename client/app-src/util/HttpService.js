ba396f73ab3777d861b2598427b9befbfb50976b
thallesManjaterra <thalles.manjaterra@gmail.com>
2017-11-06 21:08:07 +0000

    Simplificando requisições ajax com fetch api.

--- client/app-src/util/HttpService.js
+++ client/app-src/util/HttpService.js
@@ -1,18 +1,13 @@
 export class HttpService {
+    _handleErrors(res) {
+        // se não estiver ok, lança a exceção
+        if(!res.ok) throw new Error(res.statusText);
+        // se nenhum exceção foi lançada, retorna a própria resposta
+        return res;
+    }
     get(url) {
-        return new Promise((resolve, reject) => {
-            let xhr = new XMLHttpRequest();
-            xhr.open('GET', url);
-            xhr.onreadystatechange = () => {
-                if(xhr.readyState === 4) {
-                    if(xhr.status === 200) {
-                        resolve(JSON.parse(xhr.responseText));
-                    } else {
-                        reject(xhr.responseText);
-                    }
-                }
-            }
-            xhr.send(); // executa a requisição configurada
-        })
+        return fetch(url)
+        .then(res => this._handleErrors(res))
+        .then(res => res.json());
     }
 }
