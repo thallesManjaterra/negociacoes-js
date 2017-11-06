9675bc7abcb97f1447be669211433348905dfcb2
thallesManjaterra <thalles.manjaterra@gmail.com>
2017-11-06 16:08:00 +0000

    Barrel - Simplificando a importação de módulos.

--- metodosFuncao/tools/clear.js
+++
@@ -1,1 +0,0 @@
-module.exports = '\033c' //clear
--- metodosFuncao/tools/title.js
+++
@@ -1,7 +0,0 @@
-let barra = new Array(60).join('-');
-
-module.exports = (title) => {
-	console.log(`\n${barra}`);
-	console.log(title);
-	console.log(`${barra}`)
-};
